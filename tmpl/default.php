8d253e5fe8acd65f96c14cb7fb282cf5cd6e0857
Mardink Webdesign <support@mardinkwebdesign.com>
2013-04-13 22:19:02 +0200

    Added different social sites and icons

--- tmpl/default.php
+++ tmpl/default.php
@@ -8,10 +8,39 @@
 	if ($langSite != '') {
 		$langSite .= '/';
 	}
+	// Get the parameters
+	$size = $params->get('size');
 	$twitterlink = $params->get('twitterlink');
-	?>
-	<a href="<?php echo $twitterlink;?>"><img src="./modules/mod_mdsocial/assets/twitter48x48.png" /></a>
-	
-		</div>
+	$twitter = $params->get('twitter');
+	$facebooklink = $params->get('facebooklink');
+	$facebook = $params->get('facebook');
+	$githublink = $params->get('githublink');
+	$github = $params->get('github');
+	$joomlalink = $params->get('joomlalink');
+	$joomla = $params->get('joomla');
+	$linkedinlink = $params->get('linkedinlink');
+	$linkedin = $params->get('linkedin');
+	//Show twitter icon
+	if ($twitter=="1"){?>
+		<a href="<?php echo $twitterlink;?>"><img src="./modules/mod_mdsocial/assets/twitter<?php echo $size;?>.png" /></a>
+		<?php }
+	//Show Facebook Icon
+	if ($facebook=="1"){?>
+		<a href="<?php echo $facebooklink;?>"><img src="./modules/mod_mdsocial/assets/facebook<?php echo $size;?>.png" /></a>
+		<?php }
+	//Show Github Icon
+	if ($github=="1"){?>
+		<a href="<?php echo $githublink;?>"><img src="./modules/mod_mdsocial/assets/github<?php echo $size;?>.png" /></a>
+		<?php }	
+	//Show Joomla! Extension icon
+	if ($joomla=="1"){?>
+		<a href="<?php echo $joomlalink;?>"><img src="./modules/mod_mdsocial/assets/joomla<?php echo $size;?>.png" /></a>
+		<?php }	
+	//Show LinkedIn icon
+	if ($linkedin=="1"){?>
+		<a href="<?php echo $linkedinlink;?>"><img src="./modules/mod_mdsocial/assets/linkedin<?php echo $size;?>.png" /></a>
+		<?php }	
+		?>
+	</div>
 	
 	
