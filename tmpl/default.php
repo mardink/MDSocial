b5861558f116662698be4654d1820a77aba66145
Mardink Webdesign <support@mardinkwebdesign.com>
2013-04-13 22:37:43 +0200

    Added option for target blank language update

--- tmpl/default.php
+++ tmpl/default.php
@@ -20,27 +20,34 @@
 	$joomla = $params->get('joomla');
 	$linkedinlink = $params->get('linkedinlink');
 	$linkedin = $params->get('linkedin');
+	$target =$params->get('target');
+	$target_blank = 'target="_blank"';
+	if ($target=="1"){
+		$target_blank = 'target="_blank"';
+		} else {
+		$target_blank = '';
+		}
 	//Show twitter icon
 	if ($twitter=="1"){?>
-		<a href="<?php echo $twitterlink;?>"><img src="./modules/mod_mdsocial/assets/twitter<?php echo $size;?>.png" /></a>
+		<a href="<?php echo $twitterlink;?>" <?php echo $target_blank;?>><img src="./modules/mod_mdsocial/assets/twitter<?php echo $size;?>.png" /></a>
 		<?php }
 	//Show Facebook Icon
 	if ($facebook=="1"){?>
-		<a href="<?php echo $facebooklink;?>"><img src="./modules/mod_mdsocial/assets/facebook<?php echo $size;?>.png" /></a>
+		<a href="<?php echo $facebooklink;?>" <?php echo $target_blank;?>><img src="./modules/mod_mdsocial/assets/facebook<?php echo $size;?>.png" /></a>
 		<?php }
 	//Show Github Icon
 	if ($github=="1"){?>
-		<a href="<?php echo $githublink;?>"><img src="./modules/mod_mdsocial/assets/github<?php echo $size;?>.png" /></a>
+		<a href="<?php echo $githublink;?>" <?php echo $target_blank;?>><img src="./modules/mod_mdsocial/assets/github<?php echo $size;?>.png" /></a>
 		<?php }	
 	//Show Joomla! Extension icon
 	if ($joomla=="1"){?>
-		<a href="<?php echo $joomlalink;?>"><img src="./modules/mod_mdsocial/assets/joomla<?php echo $size;?>.png" /></a>
+		<a href="<?php echo $joomlalink;?>" <?php echo $target_blank;?>><img src="./modules/mod_mdsocial/assets/joomla<?php echo $size;?>.png" /></a>
 		<?php }	
 	//Show LinkedIn icon
 	if ($linkedin=="1"){?>
-		<a href="<?php echo $linkedinlink;?>"><img src="./modules/mod_mdsocial/assets/linkedin<?php echo $size;?>.png" /></a>
+		<a href="<?php echo $linkedinlink;?>" <?php echo $target_blank;?>><img src="./modules/mod_mdsocial/assets/linkedin<?php echo $size;?>.png" /></a>
 		<?php }	
-		?>
+			?>
 	</div>
 	
 	
